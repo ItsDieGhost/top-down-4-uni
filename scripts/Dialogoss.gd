75262ce03f0d0a6f3cd5d0a867f776280602aba8
Diego <darbelaezroballo@gmail.com>
2025-02-04 20:09:03 -0400

    asdadada
    dadsadasd

--- scripts/Dialogoss.gd
+++ scripts/Dialogoss.gd
@@ -4,11 +4,13 @@ var Phill: bool = false
 const npc:Dictionary = {
 	'cap1':{
 		'pd': ["Phill: Hi Jude.",
-		 "Phill: May i help you??",
-		 "Jude: No se hablar Ingles ",
-		 "Phill: No te preocupes, yo te enseño" ,
-		 "Phill: Para aprender necesitas saber las palabras basicas",
-		" "
+		"Phill: May i help you??",
+		"Jude: No se hablar Ingles ",
+		"Phill: No te preocupes, yo te enseño" ,
+		"Phill: Para aprender necesitas saber las palabras basicas",
+		"Phill: Una Buena tecnica para aprender.. ",
+		"Es usando Referencias.",
+		"Phill: "
 		
 		],
 	}
